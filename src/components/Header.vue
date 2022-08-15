0e25de4289025e58c5883c25b17b18ceeaa406a1
Mei Yi <ant19950601@gmail.com>
2022-08-15 23:57:02 +0800

    :tada: add Header component

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { MoonIcon } from "@zhuowenli/vue-feather-icons";
+</script>
+
+<template>
+   <header id="Header">
+      <ul class="header-nav">
+         <li>GitHub</li>
+         <li><MoonIcon size="1.5x" fill="currentColor" /></li>
+      </ul>
+   </header>
+</template>
