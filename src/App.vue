65bead72700d52a6785e7fa3095bdcb4614bfbf5
Mei Yi <ant19950601@gmail.com>
2022-08-18 21:31:53 +0800

    :tada: add Footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Index from "./page/Index.vue";
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
    <Header />
    <Index />
+   <Footer />
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const bEnabled = ref<boolean>(false);
+
+const chagneEnable = function () {
+   bEnabled.value = !bEnabled.value;
+};
+</script>
+
+<template>
+   <footer class="flex items-center flex-col text-sm">
+      <p>
+         Made by @
+         <a href="https://github.com/MeganxLi" class="text-light-2C698D visited:text-light-2C698D">Megan Lee</a>
+      </p>
+   </footer>
+</template>
