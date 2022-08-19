af50abcff30258bde2e52222c9c07a29db60d5f8
Mei Yi <ant19950601@gmail.com>
2022-08-19 22:07:55 +0800

    :tada: add search input UI

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { SearchIcon, XIcon } from "@zhuowenli/vue-feather-icons";
 import { ref } from "vue";
 </script>
 
-<template>page</template>
+<template>
+   <main>
+      <div class="serch-container">
+         <span class="search-icon"><SearchIcon size="1.2x" /></span>
+         <input type="text" placeholder="搜尋台灣銀行" class="search-input placeholder:text-slate-400" />
+         <span class="close-icon cursor-pointer"><XIcon size="1.2x" /></span>
+      </div>
+      <div>list</div>
+   </main>
+</template>
