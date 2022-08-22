2b948febbae6cb2fd4dad4603f3411881c746f8d
Mei Yi <ant19950601@gmail.com>
2022-08-22 12:06:44 +0800

    :memo: add serch input hidden

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
 import { SearchIcon, XIcon } from "@zhuowenli/vue-feather-icons";
 import { ref } from "vue";
+
+const serchVal = ref<string>();
 </script>
 
 <template>
    <main>
       <div class="serch-container">
          <span class="search-icon"><SearchIcon size="1.2x" /></span>
-         <input type="text" placeholder="搜尋台灣銀行" class="search-input placeholder:text-slate-400" />
-         <span class="close-icon cursor-pointer"><XIcon size="1.2x" /></span>
+         <input
+            type="text"
+            placeholder="搜尋台灣銀行"
+            class="search-input placeholder:text-slate-400"
+            v-model="serchVal"
+         />
+         <span
+            class="clean-icon cursor-pointer"
+            @click="serchVal = undefined"
+            :class="{ '!hidden': serchVal === undefined }"
+            ><XIcon size="1.2x"
+         /></span>
       </div>
-      <div>list</div>
+      <div>
+         {{ serchVal }}
+      </div>
    </main>
 </template>
