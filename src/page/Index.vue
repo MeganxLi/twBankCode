8e03007523e55bb3e0cbef08cc2c8933abbbce60
Mei Yi <ant19950601@gmail.com>
2022-08-29 22:31:47 +0800

    :construction: Bank code filter v2

--- src/page/Index.vue
+++ src/page/Index.vue
@@ -13,12 +13,18 @@ const serchVal = () => {
    if (searchInput.value !== undefined) searchInput.value.trim().toLowerCase();
 };
 
+const flatten = (input) => {
+   if (typeof input === "object") {
+      return (Array.isArray(input) ? input : Object.values(input)).reduce((acc, x) => acc.concat(flatten(x)), []);
+   } else {
+      return [input];
+   }
+};
+
 const BankCodeFiltered = () => {
    if (searchInput.value === undefined) return BankCodeSort;
    return BankCodeSort.filter((list: BankCodeType) => {
-      return Object.keys(list).some((item: string) =>
-         list[item].toLowerCase().includes(searchInput.value.trim().toLowerCase())
-      );
+      return flatten(list).includes(searchInput.value.trim().toLowerCase());
    });
 };
 </script>
