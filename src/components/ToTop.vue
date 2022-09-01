0f937ba5ff29b2f7b089bdd24665189dde1a0886
Mei Yi <ant19950601@gmail.com>
2022-09-01 22:38:00 +0800

    :tada: add Totop button

---
+++ src/components/ToTop.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { ChevronUpIcon } from "@zhuowenli/vue-feather-icons";
+</script>
+
+<template>
+   <a id="ToTop">
+      <ChevronUpIcon size="1.8x" class="up-icon" />
+   </a>
+</template>
