233eee854525833b94d398b04e27153550300a7c
Mei Yi <ant19950601@gmail.com>
2022-09-02 19:16:21 +0800

    :construction: ToTop js

--- src/components/ToTop.vue
+++ src/components/ToTop.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { ChevronUpIcon } from "@zhuowenli/vue-feather-icons";
+
+const isVisible = ref<boolean>(false);
 </script>
 
 <template>
-   <a id="ToTop">
+   <a id="ToTop" href="#Header">
       <ChevronUpIcon size="1.8x" class="up-icon" />
    </a>
 </template>
