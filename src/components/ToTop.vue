3e4cc7b3f0e7baeda40e75cad7a6363bbd119b22
Mei Yi <ant19950601@gmail.com>
2022-09-07 21:20:48 +0800

    :memo: update To Top js

--- src/components/ToTop.vue
+++ src/components/ToTop.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import { ChevronUpIcon } from "@zhuowenli/vue-feather-icons";
+</script>
 
-const isVisible = ref<boolean>(false);
+<script lang="ts">
+export default {
+   data() {
+      return {
+         windowOffsetTop: 0,
+      };
+   },
+   computed: {
+      isShow() {
+         return this.windowOffsetTop >= 200;
+      },
+   },
+   mounted() {
+      this.windowOffsetTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      window.addEventListener("scroll", () => {
+         this.windowOffsetTop = window.pageYOffset || document.documentElement.scrollTop;
+      });
+   },
+   methods: {
+      scrollToTop() {
+         const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+         if (scrollTop > 0) {
+            window.requestAnimationFrame(this.scrollToTop);
+            window.scrollTo(0, scrollTop - scrollTop / 10);
+         }
+      },
+   },
+};
 </script>
 
 <template>
-   <a id="ToTop" href="#Header">
+   <a id="ToTop" v-show="isShow" @click="scrollToTop()">
       <ChevronUpIcon size="1.8x" class="up-icon" />
    </a>
 </template>
